a88ee53cade075f2e06110b3ae6593c7f8b01c76
TomiXRM <chocolate0785lego@icloud.com>
2020-06-04 03:05:50 +0900

    add pages

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName('ouen-forward').onclick = function () {
+        console.log("click");
+}
+
+document.getElementById('ouen-forward').addEventListener('click', function () {
+        console.log("click");
+}, false);
+
+function a() {
+        console.log("click");
+}
+
+// a();
