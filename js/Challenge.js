049a3c088f67aeec9a75ccd9f23d3b6b8b4a9372
TomiXRM <chocolate0785lego@icloud.com>
2020-06-13 23:29:57 +0900

    add challenge

---
+++ js/Challenge.js
@@ -0,0 +1,211 @@
+/* カードの中身
+   ちゃんと実装するときはfetchかなにかでJSONを引っ張ってくるべきかと。 */
+const cardDataList = [
+        {
+                title: "新たな一歩",
+                body: "私は今までお母さんに任せっきりだったお弁当作りにチャレンジしてみてました。これはいつも仕事を頑張ってくれているお父さんに作りました工夫した点は、お仕事を頑張ってと言う応援メッセージとして『ファイト』と海苔でメッセージを作った点です。苦手意識が強かった卵焼きにもチャレンジしました。日頃の感謝も伝えられたので良かったです！",
+                like: "https://www.instagram.com/p/CBQnyUiJDTl/"
+        },
+        {
+                title: "はじめてのまともなクッキング",
+                body: "高校3年<br><br>ドリアを作りました。昨日のご飯がスパゲッティだったんですけど、ミートソースが余ってしまいどうせだったらまともな料理した事なかったししてみよー！ってことでミートソースを使ったドリアを作りました。コンソメとか使ったんですけど何が何かわからなかったです。でも美味しかったからOKです！",
+                like: "https://www.instagram.com/p/CBQnz4rp1rB/"
+        },
+        {
+                title: "初オールミミールクッキー",
+                body: "高校3年<br><br>オートミールクッキーを初めて作りました。今の状況で外に出ることがほとんどなくなり、運動量が減った人が多いと思います。私もそのうちの一人で、自粛前よりも体重が増えてしまいました。「痩せよう！」と決意し、ご飯を食べることをやめようとしたのですが、学校再開を迎えようとしている今、第二波に備え、免疫力を高めるために食事は取らないといけないと思いました。そんな時オートミールに出会いました。オートミールはダイエット効果があるため、日本でも徐々に人気が高まってきています。私はクッキーがすきなので、なんの苦もなく食べられました。料理の知識は全く無いですが、これを機に食に気を遣って行こうと思います！",
+                like: "https://www.instagram.com/p/CBQn0QwpZcq/"
+        },
+        {
+                title: "妹たちとお菓子作りに挑戦してみた！",
+                body: "高校2年<br><br>私は普段部活で料理をする機会が全然なかったので、私はこの休みにお菓子作りをすることを決めました。作ったのはアーモンドタルトとソーダゼリーです。途中材料を間違えてしまうというハプニングもありましたが、なんとか完成しました。工夫したのは余ったアーモンドタルト生地でサブレも作ったことです。とても楽しく料理できました。初めて２つの料理を並行して作ったのですが２つとも成功したのはとても嬉しかったです。次はご飯作りに挑戦したいです。",
+                like: "https://www.instagram.com/p/CBQo6w5pcNI/"
+        },
+        {
+                title: "点心を始めて、食事観が転身！",
+                body: "私は今まで料理をしたことがありませんでした。ですが、せっかく自由に使える時間がでたので何かやってみようと思い、難易度が高めの餃子に挑戦してみました！<br>また、お店が閉まっているため余っていた良いお肉を使ったので、僅かながらお役に立てたと思います！初めては、真っ黒に焦げたり、皮が剥がれたりしてしまいましたが、5月中に15回も餃子を作り、画像の通り綺麗に焼くことができました！この経験の中で他の料理にも興味が湧き、食べる時も作る側の視点を知ったため、食事がよりいっそう楽しくなりました！挑戦して良かったと心から思います！",
+                like: "https://www.instagram.com/p/CBQo7Ljp7nO/"
+        },
+        {
+                title: "感謝を届ける",
+                body: "毎日、会社のある父のためにお弁当を持っていってます。お弁当の中身は妹が作っています。最初は母と協力しながら作っていましたが今はすっかり一人で作れるようになっています。私はstay homeの中気晴らしで外に行く良い機会なので毎日欠かさず、家に来ている留学生と一緒に届けに行きます。お弁当を持っていくと父はとても喜んでくれます。この機会にいつも私たち家族のために働いてくれている父に感謝の気持ちを届けたいです。",
+                like: "https://www.instagram.com/p/CBQo7qnphCh/"
+        },
+
+        {
+                title: "いただきます",
+                body: "高校3年<br><br>見るのも食べるのも大好きな魚。外出自粛期間中に、自分でイチから調理することにチャレンジしてみました！刃を入れるとき、泳いでいた姿そのままの魚を前にして若干の緊張を覚えました。「命」をいただく、ということをこれほど意識したことはなかったように思います。「いただきます」と無意識に呟きながら調理していました。コアジは大好物のアジフライに、イサキは煮付けに。食卓から、旬を、季節を感じることができました。<br><br>今回、ネット上で捌き方や美味しい調理方法も学べるので、初めてでも挑戦できることが分かりました。今回と同じおまかせ鮮魚パックを毎週お願いすることにしたので、これからの旬の魚との出会いが楽しみです！",
+                like: "https://www.instagram.com/p/CBTJyCxpDxX/"
+        },
+        {
+                title: "料理等番Bot",
+                body: "高校3年<br><br>コロナ休校で家族が全員家にいる期間が長く続きました。「ゴミ出して」「昼ごはん誰が作る？」「お風呂洗って」という風に、家事を誰がやるのかと僕の家ではしばし問題になります。皆さんの家庭でもあるのではないでしょうか。そんな家事の分担で揉める生活はもうやめましょう。このLINE公式アカウント「料理当番Bot」を友達登録し、ボタンを押したり、家族ライングループに招待して「当番発表」と送信するだけで勝手にルーレットを回して通知してくれます。Botを僕の家族グループに招待してからと言うもの、みんなが役割をこのBotで決めるようになり、家事分担の揉め事は１つも無くなりました。ゲーム感覚で利用することができるのでおすすめです。",
+                like: "https://www.instagram.com/p/CBTJyiWJg4g/"
+        },
+        {
+                title: "H2Aロケット",
+                body: "高校1年<br><br>家にうまく消せない消しゴムがいくつかあったので、私は今回カッターを使って消しゴムアートに挑戦しました。私は宇宙が好きなので、ロケットを作りました。ただ細かい作業が苦手で、集中力があまりないので、１回目に作ったときは横の小さい部品が取れてしまったり色を綺麗に塗れなくて苦労しましたが、3回目に作ってやっと上の写真のような形にまでなりました！消しゴムアートは思ったより難しくて時間がかかりましたが、自分が納得するまで作れたので楽しかったし、集中力にも少し自信がつきました。細かい作業はもう少し上手になるように頑張って、また挑戦したいと思います！",
+                like: "https://www.instagram.com/p/CBTJy9fpwgE/"
+        },
+        {
+                title: "2分間のケガ予防体操",
+                body: "高校3年<br><br>COVID-19の影響で自宅で過ごす人が多いため運動する機会が減っています。そこで終息後の突然の運動・スポーツで怪我をしないために自宅で簡単にできる「2分間のケガ予防体操」というものを考案しました！！自分自身が前十字靭帯を損傷し、手術後8ヶ月のリバビリトレーニングの中でトレーナーさんや理学療法士さんから教わったことを元に、“誰でも楽しくケガ予防”にこだわって子供も高齢者の方でもやりやすいよう音に合わせた体操にしました！体操を作ることはとても楽しく、自分の辛い経験が誰かの幸せに変わることを嬉しく思いました。この体操を広めて1人でも多くの人の怪我を予防したいです！",
+                like: "https://www.instagram.com/p/CBTKmKdJV0h/"
+        },
+        {
+                title: "おうち時間で世界を平和に‼︎",
+                body: "コロナウイルスの影響で、おうち時間を楽しく過ごす方法が模索されました。そのなかでぬりえを楽しむ人が増えました。そのぬり絵を、国際協力のために使えないか、楽しんでいるだけでこの閉鎖的な時期に国際協力ができないか、と考えて「ぬりえで協力!!プロジェクト」を始めました。このプロジェクトではぬりえを塗ってもらい、スキャンして送ってもらったものを私たちが切り貼りして、バングラデシュのこどもたちが数字の概念を学べる教材にします。現在、多くの方に参加していただいて、28冊が完成しました!直接コロナウイルスの状況に関係することではないですが、このコロナが終わったときに、バングラデシュのこどもたちがまた質の高い教育を受けられる第一歩を作れたのではないかと思います!!<br><br><a href='https://sites.google.com/view/kikkakedukuri2020/%E3%83%9B%E3%83%BC%E3%83%A0/%E3%81%AC%E3%82%8A%E3%81%88%E3%81%A7%E5%8D%94%E5%8A%9B%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88'>ぬりえで協力！！ ページリンク</a>",
+                like: "https://www.instagram.com/p/CBTKndvpFA1/"
+        },
+        {
+                title: "Beyondコロナ~再興(彩光)~",
+                body: "高校3年<br><br>普段、墨絵という白黒の世界を描いているんですが、それがコロナで閑散として色を失った現状とマッチしていると感じました。そこでコロナが収束して賑わいが戻ることを願い、色を取り入れることにチャレンジしました。後ろから光を当てると色が着く仕組みになっています！描く題材も皆が上を向いていけるようライトアップされる通天閣にしました！この作品が多くの人の目にとまり少しでも明るい気持ちになってほしく応募しました！このような機会を設けていただきありがとうございます！",
+                like: "https://www.instagram.com/p/CBTKofFp7Lk/"
+        },
+        {
+                title: "私の家での研究",
+                body: "私は昔から東京ディズニーリゾートが大好きで、このコロナの機会に色んなことを知りたいと思い本やネットや辞書を使い、研究しています。例えば、イタリアがモチーフのエリアでしたらその元となった土地について調べたり、どのアトラクションがどの企業がスポンサーなのかを調べて覚えたりしています。",
+                like: "https://www.instagram.com/p/CBVseXIpjGW/"
+        },
+        {
+                title: "Thank you〜ブ",
+                body: "自分はルービックキューブが大の苦手です。そこで医療関係者の皆様への感謝、自分へのチャレンジを兼ねてやってみました。<br>このルービックキューブはもともと完成していた物にメッセージを書き、バラバラにしもう一度組み直しています。戻し方をYouTube等ネットで調べたりもしました。<br>途中で分からなくなる等のトラブルもありました。完成まで2日間かかりました。でもチャレンジして良かったです‼︎医療関係者の皆様ありがとうございます。<br>(写真は組み直した後のものです)",
+                like: "https://www.instagram.com/p/CBVsevUJ3nL/"
+        },
+        {
+                title: "Let’s think about今だからこそ出来る事",
+                body: "私は元々英語が苦手でした。高校受験の際は英語が足を引っ張ってしまい、本命の公立高校に合格することはできませんでした。そんな私ですが高校生になり、先輩や同級生が世界に出て活躍していることに刺激を受けました。それからというもの、小さいことに悩まずもっと広い舞台でに出たいと強く思うようになりました。コロナによって現在世界的な繋がりは薄れていますが、今こそ自分の語学力を高めるときだと思い、英検一級の勉強を始めました。私は2ヶ月で印刷が薄くなるほど単語帳を使い込みましたが、皆さんにもまだまだ出口の見えないコロナに負けずに、自分をみつめる時間ができた今だからこそやれる事に打ち込んで欲しいです。そんなきっかけになればと思い、今回応募させていただきました。",
+                like: "https://www.instagram.com/p/CBVsfGTJQYB/"
+        },
+        {
+                title: "パソコンへの挑戦",
+                body: "高校1年<br><br>私は休校期間中にパソコンの勉強を始めました。始めた理由は、以前からパソコンに興味があったのと、将来パソコンを使う職業に就くかもしれないと考えたからです。学校に行っているときは中々手をつけられなかったので、時間がある今は良い機会だと思いました。しかしただ勉強するだけではモチベーションが下がってきてしまうので、Microsoft Office Specialist のWordに合格することを目標にしました。今までWordを使ったことが無かったのでわからないことがたくさんありましたが、無事合格出来ました。今はExcelを勉強中です。学校が再開しても勉強を続けて、もっと色々な機能を使えるようになりたいです。",
+                like: "https://www.instagram.com/p/CBVtG13JoV2/"
+        },
+        {
+                title: "ラミネートと3Dプリンタを使用したフェイスシールドの寄附",
+                body: "中3<br><br>今回はラミネートと3Dプリンタを使用したフェイスシールドの作成に挑戦しました。ネットには3Dプリンタとクリアファイルで作成するフェイスシールドの記事はたくさんありますが、様々な素材で試作した結果、透明度が高くコストも低いラミネートを使用したフェイスシールドが最も良いと考えました。フィルムの間には医療・介護従事者の方々への応援メッセージの意味を込めて「Beyond COVID-19」の文字を書いたシールを視界を遮らないところに貼りました。そしてこのフェイスシールド30個を住んでいる市に寄附しました。今の世の中で今後も役に立てるものを作りたいと考えています。",
+                like: "https://www.instagram.com/p/CBVtHY0pJpB/"
+        },
+        {
+                title: "音楽で笑顔を輝かせよう",
+                body: "高２<br><br>家にある楽器をフルに使って音楽で元気を届けられるように1人で曲を演奏したいと思います！学校から持って帰っている物もあり、この期間だからこそ挑戦できることです。サックスが1番難しくて苦戦中ですが頑張りたいと思います。楽器の名前は<br>カホン　ベース<br>コントラバス　アルトサックス<br>アコギ　ピアノ<br>です",
+                like: "https://www.instagram.com/p/CBVtHsFpros/"
+        },
+        // {
+        //         title: "",
+        //         body: "",
+        //         like: ""
+        // },
+        // {
+        //         title: "",
+        //         body: "",
+        //         like: ""
+        // },
+        // {
+        //         title: "",
+        //         body: "",
+        //         like: ""
+        // },
+];
+
+const cardsPerPage = 3; // 3個しかデータ写してないのでとりあえず1ページあたり2個
+const pageCount = (cardDataList.length / cardsPerPage) + (cardDataList.length % cardsPerPage); // ページ数
+let page = 1;
+function initialize() {
+        /* とりあえず container-fluid 以下のカードを全削除  */
+        document.querySelectorAll("#ouen > div > div > div.card").forEach((element) => element.remove());
+
+        /* cardDataListに沿ってカードを作成 */
+        for (let i = 0; i < cardsPerPage; i++) {
+                const cardImageElement = document.createElement("img");
+                cardImageElement.classList.add("card-img", "rounded-1");
+
+                const cardImageWrapperElement = document.createElement("div");
+                cardImageWrapperElement.classList.add("col-md-4", "gallery-image");
+                cardImageWrapperElement.appendChild(cardImageElement);
+
+                const cardTitleElement = document.createElement("h5");
+                cardTitleElement.classList.add("card-title");
+
+                const cardTextElement = document.createElement("p");
+                cardTextElement.classList.add("card-text");
+
+                const likeButtonElement = document.createElement("a");
+
+                const cardBodyElement = document.createElement("div");
+                cardBodyElement.classList.add("card-body");
+                [cardTitleElement, cardTextElement, likeButtonElement].forEach((element) => cardBodyElement.appendChild(element)); // cardBodyElement.appendChild(element) を何回もやってもいいものの面倒なので。
+
+                const cardBodyWrapperElement = document.createElement("div");
+                cardBodyWrapperElement.classList.add("col-md-8");
+                cardBodyWrapperElement.appendChild(cardBodyElement);
+
+                const cardRowElement = document.createElement("div");
+                cardRowElement.classList.add("row", "no-gutters");
+                [cardImageWrapperElement, cardBodyWrapperElement].forEach((element) => cardRowElement.appendChild(element)); // cardBodyElement(以下略)
+
+                const cardElement = document.createElement("div");
+                cardElement.classList.add("card");
+                cardElement.style.maxWidth = "800px";
+                cardElement.style.margin = "30px auto";
+                cardElement.appendChild(cardRowElement);
+
+                document.querySelector("#ouen > div > div").appendChild(cardElement);
+
+                if (i < cardDataList.length) {
+                        // cardImageElement.src = cardDataList[i].image;
+                        cardImageElement.src = cardDataList[i].like + "media/?size=l";
+                        cardTitleElement.innerText = cardDataList[i].title;
+                        cardTextElement.innerText = cardDataList[i].body;
+                        likeButtonElement.href = cardDataList[i].image;
+                        likeButtonElement.innerText = cardDataList[i].like;
+                        likeButtonElement.innerHTML = "いいねする";
+                }
+        }
+}
+
+async function changePage(page) {
+        const offset = page * cardsPerPage;
+        document.querySelector("#ouen > div > div").querySelectorAll(".card").forEach((card, index) => {
+                card.style.display = cardDataList.length <= offset + index ? "none" : "block"; // データリストの最後より後になる要素はとりあえず非表示に
+                if (cardDataList.length <= offset + index) return; // データリストの最後より後は中身がないのでスキップ
+                const cardData = cardDataList[offset + index];
+
+                const cardImageElement = card.querySelector("div > div.col-md-4 > img");
+                // cardImageElement.src = cardData.image;
+                cardImageElement.src = cardData.like + "media/?size=l";
+                const cardTitleElement = card.querySelector("div > div.col-md-8 > div > h5");
+                cardTitleElement.classList.add("card-title");
+                cardTitleElement.innerText = cardData.title;
+
+                const cardTextElement = card.querySelector("div > div.col-md-8 > div > p");
+                cardTextElement.innerHTML = cardData.body;
+
+                const likeButtonElement = card.querySelector("div > div.col-md-8 > div > a");
+                likeButtonElement.href = cardData.like;
+                likeButtonElement.innerHTML = "いいねする";
+        });
+}
+
+document.querySelector(".btn-group > .prev").onclick = function () {
+        page--;
+        if (page < 1) {
+                page = pageCount;
+        }
+        console.log(page);
+        changePage(page - 1);
+        window.location.href = '#main';
+};
+
+document.querySelector(".btn-group > .next").onclick = function () {
+        page++;
+        if (page > pageCount) {
+                page = 1;
+        }
+        console.log(page);
+        changePage(page - 1);
+        window.location.href = '#main';
+};
+initialize();
+changePage(page - 1);
